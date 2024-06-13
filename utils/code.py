e1564a9a367dea2900889199ff8c0cdcef422f00
cheandrey <skyandreymann@gmail.com>
2024-06-13 14:35:34 +0300

    Добавлена функция сортировки

--- utils/code.py
+++ utils/code.py
@@ -1,5 +1,5 @@
 import json
-import datetime
+from datetime import datetime
 
 
 def load_operations():
@@ -10,5 +10,46 @@ def load_operations():
         return data
 
 
-list_line = load_operations()
-print(list_line)
+def sort_last_five(operlist):
+    """вибираем пять последних операций"""
+
+    def get_date(item):
+        return item.get('date','')
+
+    def validate_date(date_str):
+        try:
+            datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
+            return True
+        except ValueError:
+            return False
+
+
+    sorted_operations = sorted(operlist, key=lambda x: validate_date(get_date(x)), reverse=True)
+    return sorted_operations[:5]
+
+
+list_actions = load_operations()
+#print(list_actions)
+
+
+
+count = len(list_actions)
+
+print(count)
+
+i = 0
+for dict in list_actions:
+    for key in dict.keys():
+        if key == "date":
+            i += 1
+
+
+print(i)
+
+sort_last_five(list_actions)
+
+
+
+list_five = sort_last_five(list_actions)
+for operation in list_five:
+    print(operation)
