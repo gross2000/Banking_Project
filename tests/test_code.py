d03a3629551759f4176a7345c2e0913a719d7bbf
cheandrey <skyandreymann@gmail.com>
2024-06-24 00:55:20 +0300

    Доработка с сортировкой по ECECUTED

--- tests/test_code.py
+++ tests/test_code.py
@@ -7,11 +7,21 @@ from utils.code import sort_last_five
 from utils.code import date_reform
 from utils.code import from_reform
 from utils.code import to_reform
+from utils.code import execute_sort
+
 
 
 def test_load_json():
     data = load_operations()
-    assert isinstance(data, dict), "ошибка синтаксиса данного словаря"
+    assert all(isinstance(item, dict) for item in data), "ошибка синтаксиса данного словаря"
+
+
+def test_execute_sort():
+    "проверяем список сортированный по 'EXECUTED'"
+    list_actions = load_operations()
+    listing = execute_sort(list_actions)
+    if "state" in listing:
+        assert "EXECUTED" in listing
 
 
 def test_sort_last_five():
